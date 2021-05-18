dfd7c9410cbd48feb25147fa9535bd74603d5f19
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-05-18 09:44:33 +0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const app = express();
